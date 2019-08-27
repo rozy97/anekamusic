018199f2f04dfb2e8b6d4824123d778754fb0ae0
Firmansyah rozy <rozy.firmansyah@gmail.com>
2019-08-28 05:57:34 +0700

    install reactstrap & router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import SearchBox from "./components/searchbox/SearchBox";
-import Add from "./components/add/Add";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [],
+      items: [],
       searchField: ""
     };
   }
@@ -19,7 +18,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <SearchBox />
-        <Add />
+
         {/* <CardList /> */}
         <Footer />
       </div>
