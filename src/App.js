a98a5b3c45927b1d0029ec9cfc37f594e3eca2ee
Firmansyah rozy <rozy.firmansyah@gmail.com>
2019-08-28 02:08:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import SearchBox from "./components/searchbox/SearchBox";
+import Add from "./components/add/Add";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBox />
+        <Add />
+        {/* <CardList /> */}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
