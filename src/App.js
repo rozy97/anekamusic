5b99df86b4b4577b552dc147eb2927f5247491ea
Firmansyah rozy <rozy.firmansyah@gmail.com>
2019-08-28 12:10:28 +0700

    update search lagi

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import SearchBox from "./components/searchbox/SearchBox";
+import dataDummy from "./components/data-dummy/dataDummy";
+import categories from "./components/data-dummy/categories";
+import CardList from "./components/main-component/card-list";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      items: [],
+      items: dataDummy,
+      categories: categories,
       searchField: ""
     };
   }
-
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
   render() {
+    const filteredCategories = this.categories.filter(category =>
+      category.category.toLowerCase().includes(this.searchField.toLowerCase())
+    );
     return (
       <div className="App">
         <Header />
-        <SearchBox />
+        <SearchBox placeholder="Search" handleChange={this.handleChange} />
 
-        {/* <CardList /> */}
+        <CardList items={filteredCategories} />
         <Footer />
       </div>
     );
